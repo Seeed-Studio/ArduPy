ff275d0b624ca7d18a7e6b9ca420c889e1c4c834
PowerfulCat <x4495@outlook.com>
2019-08-06 16:09:19 +0800

    Use the macro 'bar' replace macro '$', to avoid misunderstand.

--- common-hal/grove/GroveLedBar.cpp
+++ common-hal/grove/GroveLedBar.cpp
@@ -1,3 +1,29 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include "submodule/Grove_LED_Bar.h"
 extern "C"{
 #include "py/mphal.h"
@@ -8,7 +34,7 @@ extern "C"{
 #include "shared-bindings/util.h"
 }
 
-#define $ (*(Grove_LED_Bar *)self)
+#define bar (*(Grove_LED_Bar *)self)
 void * operator new(size_t, void *); 
 
 namespace {
@@ -40,21 +66,21 @@ extern "C" {
         *value = dev->countOfLed();
     }
     void common_hal_led_bar_set_bits(void *self, uint32_t value){
-        $.setBits(value);
+        bar.setBits(value);
     }
     void common_hal_led_bar_get_bits(void *self, uint32_t * value){
-        *value = $.getBits();
+        *value = bar.getBits();
     }
     void common_hal_led_bar_set_level(void *self, float value){
-        $.setLevel(value);
+        bar.setLevel(value);
     }
     void common_hal_led_bar_set_brightness(void *self, uint32_t led_no, float value){
-        $.setLed(led_no, value);
+        bar.setLed(led_no, value);
     }
     void common_hal_led_bar_set_is_reverse_show(void *self, uint32_t value){
-        $.setGreenToRed(value != 0);
+        bar.setGreenToRed(value != 0);
     }
     void common_hal_led_bar_toggle(void *self, uint32_t led_no){
-        $.toggleLed(led_no);
+        bar.toggleLed(led_no);
     }
 }
