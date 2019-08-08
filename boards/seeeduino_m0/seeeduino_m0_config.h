c9e961b6e17e30e3762ab014d98c4ab0ffb9800e
PowerfulCat <x4495@outlook.com>
2019-08-08 09:10:01 +0800

    Replace the platform-related variables.

--- boards/seeeduino_m0/seeeduino_m0_config.h
+++ boards/seeeduino_m0/seeeduino_m0_config.h
@@ -1,38 +1,26 @@
 // options to control how MicroPython is built
-#define MICROPY_HW_BOARD_NAME                       "Ardupy"
-#define MICROPY_HW_MCU_NAME                         "seeed"
-#define MICROPY_PY_SYS_PLATFORM                     "Atmel SAMD21"
+#define MICROPY_HW_BOARD_NAME               "Ardupy"
+#define MICROPY_HW_MCU_NAME                 "seeed"
+#define MICROPY_PY_SYS_PLATFORM             "Atmel SAMD21"
 #define ARDUPY_FULL_BUILD                   (0)
-#define MICROPY_USE_INTERNAL_PRINTF (1)
-#define MICROPY_EMIT_THUMB          (0)
-#define MICROPY_EMIT_INLINE_THUMB   (0)
+#define MICROPY_USE_INTERNAL_PRINTF         (1)
+#define MICROPY_EMIT_THUMB                  (0)
+#define MICROPY_EMIT_INLINE_THUMB           (0)
 #define MICROPY_PY_COLLECTIONS_ORDEREDDICT  (0)
-
-
-
-
-#define MICROPY_HW_LED_STATUS   (&pin_PA17)
-
-#define MICROPY_HW_LED_TX   &pin_PA27
-#define MICROPY_HW_LED_RX   &pin_PB03
-
-// #define MICROPY_PORT_A        (PORT_PA27)
-// #define MICROPY_PORT_B        (PORT_PB03)
-// #define MICROPY_PORT_C        (0)
 
 // #define CIRCUITPY_INTERNAL_NVM_SIZE 256 
 
 // #define BOARD_FLASH_SIZE (0x00040000 - 0x2000 - 0x010000 - CIRCUITPY_INTERNAL_NVM_SIZE)
 
-#define DEFAULT_I2C_BUS_SCL (&pin_PA23)
-#define DEFAULT_I2C_BUS_SDA (&pin_PA22)
+#define DEFAULT_I2C_BUS_SCL                 (&pin_SCL)
+#define DEFAULT_I2C_BUS_SDA                 (&pin_SDA)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_PB11)
-#define DEFAULT_SPI_BUS_MOSI (&pin_PB10)
-#define DEFAULT_SPI_BUS_MISO (&pin_PA12)
+#define DEFAULT_SPI_BUS_SCK                 (&pin_SCK)
+#define DEFAULT_SPI_BUS_MOSI                (&pin_MOSI)
+#define DEFAULT_SPI_BUS_MISO                (&pin_MISO)
 
-#define DEFAULT_UART_BUS_RX (&pin_PA11)
-#define DEFAULT_UART_BUS_TX (&pin_PA10)
+#define DEFAULT_UART_BUS_RX                 (&pin_RX)
+#define DEFAULT_UART_BUS_TX                 (&pin_TX)
 
 // USB is always used internally so skip the pin objects for it. 
 #define IGNORE_PIN_PA24     1   
