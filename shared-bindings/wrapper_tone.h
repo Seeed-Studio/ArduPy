7dd397ae6308209cab6e756c06b71417e6998a1a
Baozhu Zuo <baozhu.zuo@gmail.com>
2020-03-23 11:08:44 +0800

    add pwm API, simple use pin and set freq and duty

---
+++ shared-bindings/wrapper_tone.h
@@ -0,0 +1,12 @@
+
+
+
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+void wrapper_tone(uint32_t _pin, uint32_t frequency, uint32_t duration);
+void wrapper_notone(uint32_t _pin);
+#ifdef __cplusplus
+}
+#endif
