f3d2f77614b2ac1e7f85d0a2e5d9fda6299f93c3
Baozhu Zuo <zuobaozhu@gmail.com>
2020-05-31 11:40:36 +0800

    New add part family folder

--- release_arduinocore.sh
+++ release_arduinocore.sh
@@ -16,9 +16,11 @@ make install DESTDIR=$tmp_dir
 
 mv ${tmp_dir}/usr/local/* ${work_pwd}/deploy
 mv  ${work_pwd}/deploy/ArduPy/lib/libmicropython.a   ${work_pwd}/deploy/ArduPy/lib/libmicropython-cortexm0plus.a
-mkdir -p  ${work_pwd}/deploy/Seeeduino/tools/seeeduino_m0 
-mv  ${work_pwd}/deploy/Seeeduino/tools/genhdr/* ${work_pwd}/deploy/Seeeduino/tools/seeeduino_m0 
-mv ${work_pwd}/deploy/Seeeduino/tools/seeeduino_m0  ${work_pwd}/deploy/Seeeduino/tools/genhdr
+mkdir -p  ${work_pwd}/deploy/Seeeduino/tools/samd21
+mv  ${work_pwd}/deploy/Seeeduino/tools/genhdr/* ${work_pwd}/deploy/Seeeduino/tools/samd21
+mv ${work_pwd}/deploy/Seeeduino/tools/samd21 ${work_pwd}/deploy/Seeeduino/tools/genhdr
+mv  ${work_pwd}/deploy/ArduPy/boards/seeeduino_m0 ${work_pwd}/deploy/ArduPy/boards/samd21
+
 
 rm -rf *
 cmake ../ -DARDUINO_CORE_PATH=$HOME/.arduino15/packages/Seeeduino/hardware/samd/$COREVER -DTOOLCHAIN=$HOME/.arduino15/packages/Seeeduino/tools/arm-none-eabi-gcc/7-2017q4/bin -DBOARD=wio_terminal -DARDUINO_VERIANT=wio_terminal
@@ -28,8 +30,10 @@ make install DESTDIR=$tmp_dir
 
 
 cp $tmp_dir/usr/local/ArduPy/lib/libmicropython.a ${work_pwd}/deploy/ArduPy/lib/libmicropython-cortexm4lf.a
-mkdir -p ${work_pwd}/deploy/Seeeduino/tools/genhdr/wio_terminal
-cp -rf $tmp_dir/usr/local/Seeeduino/tools/genhdr/* ${work_pwd}/deploy/Seeeduino/tools/genhdr/wio_terminal
+mkdir -p ${work_pwd}/deploy/Seeeduino/tools/genhdr/samd51
+cp -rf $tmp_dir/usr/local/Seeeduino/tools/genhdr/* ${work_pwd}/deploy/Seeeduino/tools/genhdr/samd51
+mv  ${work_pwd}/deploy/ArduPy/boards/wio_terminal ${work_pwd}/deploy/ArduPy/boards/samd51
+
 
 cd ${work_pwd}/deploy/
 tar -jcf  ardupy-core-${version}.tar.bz2 ArduPy  Seeeduino
