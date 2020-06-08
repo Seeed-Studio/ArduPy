d6820e214894bc9d256c6e304ce06af227a24faf
LynnL4 <lht856@foxmail.com>
2020-06-08 12:34:40 +0800

    modify xiao board.cmake

--- ports/samd/boards/xiao/board.cmake
+++ ports/samd/boards/xiao/board.cmake
@@ -6,42 +6,29 @@ set(micropython_CFLAGS
         -I${ARDUINO_CMSIS_ATMEL_PATH}
 )
 
-include_directories(${ARDUINO_CORE_PATH}/libraries/HID)
-#include_directories(${ARDUINO_CORE_PATH}/libraries/I2S/src)
-include_directories(${ARDUINO_CORE_PATH}/libraries/SAMD_AnalogCorrection/src)
-include_directories(${ARDUINO_CORE_PATH}/libraries/USBHost/src)
 include_directories(${ARDUINO_CMSIS_PATH}/CMSIS/Include)
-# include_directories(${ARDUINO_CMSIS_ATMEL_PATH})
-include_directories(${ARDUPY_BOARD_PATH}/asf4/hal/include)
-include_directories(${ARDUPY_BOARD_PATH}/asf4/hal/utils/include)
-include_directories(${ARDUPY_BOARD_PATH}/asf4/hpl/nvmctrl)
-include_directories(${ARDUPY_BOARD_PATH}/asf4/hpl/pm)
-include_directories(${ARDUPY_BOARD_PATH}/asf4/samd21a/include)
-include_directories(${ARDUPY_BOARD_PATH}/asf4/hri)
-include_directories(${ARDUPY_BOARD_PATH}/asf4/config)
+include_directories(${ARDUPY_LIB_PATH}/asf4/hal/include)
+include_directories(${ARDUPY_LIB_PATH}/asf4/hal/utils/include)
+include_directories(${ARDUPY_LIB_PATH}/asf4/hpl/nvmctrl)
+include_directories(${ARDUPY_LIB_PATH}/asf4/hpl/pm)
+include_directories(${ARDUPY_LIB_PATH}/asf4/samd21a/include)
+include_directories(${ARDUPY_LIB_PATH}/asf4/hri)
+include_directories(${ARDUPY_LIB_PATH}/asf4/config)
 include_directories(${ARDUINO_CORE_PATH}/cores/arduino/TinyUSB)
 include_directories(${ARDUINO_CORE_PATH}/cores/arduino/TinyUSB/Adafruit_TinyUSB_ArduinoCore/)
 include_directories(${ARDUINO_CORE_PATH}/cores/arduino/TinyUSB/Adafruit_TinyUSB_ArduinoCore/tinyusb/src)
 
 
 add_source_files(
-        ${ARDUINO_CORE_PATH}/libraries/HID/*.c
-        ${ARDUINO_CORE_PATH}/libraries/HID/*.cpp
-	#${ARDUINO_CORE_PATH}/libraries/I2S/src/*.c
-	#${ARDUINO_CORE_PATH}/libraries/I2S/src/*.cpp
-        ${ARDUINO_CORE_PATH}/libraries/SAMD_AnalogCorrection/src/*.c
-        ${ARDUINO_CORE_PATH}/libraries/SAMD_AnalogCorrection/src/*.cpp
-        ${ARDUINO_CORE_PATH}/libraries/USBHost/src/*.c
-        ${ARDUINO_CORE_PATH}/libraries/USBHost/src/*.cpp
         ${ARDUINO_CORE_PATH}/cores/arduino/*.s
         ${ARDUINO_CORE_PATH}/cores/arduino/*.S
 )
 
 set(BOARD_SRC ${BOARD_SRC}  
         ${ARDUPY_BOARD_PATH}/mphalport.c
-        ${ARDUPY_BOARD_PATH}/asf4/hal/src/hal_flash.c
-        ${ARDUPY_BOARD_PATH}/asf4/hpl/nvmctrl/hpl_nvmctrl.c
-        ${ARDUPY_BOARD_PATH}/asf4/hal/utils/src/utils_assert.c
+        ${ARDUPY_LIB_PATH}/asf4/hal/src/hal_flash.c
+        ${ARDUPY_LIB_PATH}/asf4/hpl/nvmctrl/hpl_nvmctrl.c
+        ${ARDUPY_LIB_PATH}/asf4/hal/utils/src/utils_assert.c
 )
 set(BOARD_DEF   -DF_CPU=48000000L 
                 -DARDUINO=10809 
