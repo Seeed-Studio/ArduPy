cddc1dd18186a7a7709c7d3d183e932552de74c7
LynnL4 <lht856@foxmail.com>
2020-07-03 16:04:01 +0800

    Fix spelling errors

--- ports/samd/release_ardupycore.sh
+++ ports/samd/release_ardupycore.sh
@@ -10,13 +10,13 @@ work_pwd=$(pwd)
 rm -rf deploy || true
 mkdir -p deploy
 
-cd build && cmake ../ -DARDUINO_CORE_PATH=$HOME/.arduino15/packages/Seeeduino/hardware/samd/$COREVER -DTOOLCHAIN=$HOME/.arduino15/packages/Seeeduino/tools/arm-none-eabi-gcc/7-2017q4/bin -DBOARD=xiao -DARDUINO_VERIANT=XIAO_m0 
+cd build && cmake ../ -DARDUINO_CORE_PATH=$HOME/.arduino15/packages/Seeeduino/hardware/samd/$COREVER -DTOOLCHAIN=$HOME/.arduino15/packages/Seeeduino/tools/arm-none-eabi-gcc/7-2017q4/bin -DBOARD=xiao -DARDUINO_VARIANT=XIAO_m0 
 make install DESTDIR=$tmp_dir 
 
 mv ${tmp_dir}/usr/local/* ${work_pwd}/deploy
 
 rm -rf *
-cmake ../ -DARDUINO_CORE_PATH=$HOME/.arduino15/packages/Seeeduino/hardware/samd/$COREVER -DTOOLCHAIN=$HOME/.arduino15/packages/Seeeduino/tools/arm-none-eabi-gcc/7-2017q4/bin -DBOARD=wio_terminal -DARDUINO_VERIANT=wio_terminal
+cmake ../ -DARDUINO_CORE_PATH=$HOME/.arduino15/packages/Seeeduino/hardware/samd/$COREVER -DTOOLCHAIN=$HOME/.arduino15/packages/Seeeduino/tools/arm-none-eabi-gcc/7-2017q4/bin -DBOARD=wio_terminal -DARDUINO_VARIANT=wio_terminal
 rm -rf $tmp_dir 
 
 make install DESTDIR=$tmp_dir 
@@ -25,6 +25,7 @@ make install DESTDIR=$tmp_dir
 cp -rf $tmp_dir/usr/local/core/ArduPy/boards/wio_terminal/libmicropython.a ${work_pwd}/deploy/core/ArduPy/boards/wio_terminal/
 cp -rf $tmp_dir/usr/local/core/ArduPy/genhdr/wio_terminal ${work_pwd}/deploy/core/ArduPy/genhdr/
 cp -rf $tmp_dir/usr/local/core/ArduPy/boards/wio_terminal ${work_pwd}/deploy/core/ArduPy/boards/
+
 
 
 cd ${work_pwd}/deploy/
